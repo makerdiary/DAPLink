fadb08b8ce3896aa9417d462824f09e306eefa6d
Yihui Xiong <yihui.xiong@hotmail.com>
2020-03-23 14:37:54 +0800

    simplify circular buffer

--- source/daplink/circ_buf.c
+++ source/daplink/circ_buf.c
@@ -27,53 +27,58 @@
 
 void circ_buf_init(circ_buf_t *circ_buf, uint8_t *buffer, uint32_t size)
 {
-    cortex_int_state_t state;
-    state = cortex_int_get_and_disable();
+    // cortex_int_state_t state;
+    // state = cortex_int_get_and_disable();
 
     circ_buf->buf = buffer;
     circ_buf->size = size;
     circ_buf->head = 0;
     circ_buf->tail = 0;
 
-    cortex_int_restore(state);
+    // cortex_int_restore(state);
 }
 
 void circ_buf_push(circ_buf_t *circ_buf, uint8_t data)
 {
-    cortex_int_state_t state;
-    state = cortex_int_get_and_disable();
+    // cortex_int_state_t state;
+    // state = cortex_int_get_and_disable();
 
-    circ_buf->buf[circ_buf->tail] = data;
-    circ_buf->tail += 1;
-    if (circ_buf->tail >= circ_buf->size) {
-        util_assert(circ_buf->tail == circ_buf->size);
-        circ_buf->tail = 0;
+    if ((circ_buf->tail - circ_buf->head) >= circ_buf->size) {
+        // buffer is full, drop data
+        return;
     }
 
-    // Assert no overflow
-    util_assert(circ_buf->head != circ_buf->tail);
+    circ_buf->buf[circ_buf->tail % circ_buf->size] = data;
+    circ_buf->tail += 1;
+    // if (circ_buf->tail >= circ_buf->size) {
+    //     util_assert(circ_buf->tail == circ_buf->size);
+    //     circ_buf->tail = 0;
+    // }
 
-    cortex_int_restore(state);
+    // Assert no overflow
+    // util_assert(circ_buf->head != circ_buf->tail);
+
+    // cortex_int_restore(state);
 }
 
 uint8_t circ_buf_pop(circ_buf_t *circ_buf)
 {
     uint8_t data;
-    cortex_int_state_t state;
+    // cortex_int_state_t state;
 
-    state = cortex_int_get_and_disable();
+    // state = cortex_int_get_and_disable();
 
     // Assert buffer isn't empty
-    util_assert(circ_buf->head != circ_buf->tail);
+    // util_assert(circ_buf->head != circ_buf->tail);
 
-    data = circ_buf->buf[circ_buf->head];
+    data = circ_buf->buf[circ_buf->head % circ_buf->size];
     circ_buf->head += 1;
-    if (circ_buf->head >= circ_buf->size) {
-        util_assert(circ_buf->head == circ_buf->size);
-        circ_buf->head = 0;
-    }
+    // if (circ_buf->head >= circ_buf->size) {
+    //     util_assert(circ_buf->head == circ_buf->size);
+    //     circ_buf->head = 0;
+    // }
 
-    cortex_int_restore(state);
+    // cortex_int_restore(state);
     
     return data;
 }
@@ -81,30 +86,30 @@ uint8_t circ_buf_pop(circ_buf_t *circ_buf)
 uint32_t circ_buf_count_used(circ_buf_t *circ_buf)
 {
     uint32_t cnt;
-    cortex_int_state_t state;
+    // cortex_int_state_t state;
 
-    state = cortex_int_get_and_disable();
+    // state = cortex_int_get_and_disable();
 
-    if (circ_buf->tail >= circ_buf->head) {
+    // if (circ_buf->tail >= circ_buf->head) {
         cnt = circ_buf->tail - circ_buf->head;
-    } else {
-        cnt = circ_buf->tail + circ_buf->size - circ_buf->head;
-    }
+    // } else {
+    //     cnt = circ_buf->tail + circ_buf->size - circ_buf->head;
+    // }
 
-    cortex_int_restore(state);
+    // cortex_int_restore(state);
     return cnt;
 }
     
 uint32_t circ_buf_count_free(circ_buf_t *circ_buf)
 {
     uint32_t cnt;
-    cortex_int_state_t state;
+    // cortex_int_state_t state;
 
-    state = cortex_int_get_and_disable();
+    // state = cortex_int_get_and_disable();
 
-    cnt = circ_buf->size - circ_buf_count_used(circ_buf) - 1;
+    cnt = circ_buf->size - circ_buf_count_used(circ_buf);
 
-    cortex_int_restore(state);
+    // cortex_int_restore(state);
     return cnt;
 }
 
